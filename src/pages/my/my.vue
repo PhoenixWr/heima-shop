7954c7d8074a230e2796053700394c79859e1a54
Simon <ooosaka1214@outlook.com>
2023-10-29 22:37:49 +0800

    feat(pages): my页面添加测试请求工具模块代码

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
+import '@/utils/request' // test
 
 const memberStore = useMemberStore()
+
+// 网络请求测试
+const getData = async () => {
+  const res = await uni.request({
+    method: 'GET',
+    url: '/home/hot/mutli',
+  })
+  console.log(res)
+}
 </script>
 
 <template>
@@ -20,6 +30,7 @@ const memberStore = useMemberStore()
       保存用户信息
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <button size="mini" type="primary" @tap="getData">网络请求测试</button>
   </view>
 </template>
 

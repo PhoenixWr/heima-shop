c453cc602cb0a6d0de1aeb4c5bc60ac182a48438
Simon <ooosaka1214@outlook.com>
2023-10-31 22:18:01 +0800

    refactor(types/home.d.ts): 修改首页轮播图数据类型

--- src/types/home.d.ts
+++ src/types/home.d.ts
@@ -1,10 +1,6 @@
 // 首页数据类型声明
 
-/** 跳转类型 */
-type NavigateType = 1 | 2 | 3
-/**
- * 首页轮播图数据类型
- */
+/** 首页轮播图数据类型 */
 export type BannerItem = {
   /** id */
   id: string
@@ -13,5 +9,5 @@ export type BannerItem = {
   /** 跳转链接 */
   hrefUrl: string
   /** 跳转类型 1: 页面 2: H5 3: 小程序 */
-  type: NavigateType
+  type: number
 }

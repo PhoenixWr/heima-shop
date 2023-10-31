4d87d4149329f69ace090dbc1334473e4a754a8b
Simon <ooosaka1214@outlook.com>
2023-10-31 20:33:45 +0800

    feat(types): 添加首页数据类型声明文件

---
+++ src/types/home.d.ts
@@ -0,0 +1,17 @@
+// 首页数据类型声明
+
+/** 跳转类型 */
+type NavigateType = 1 | 2 | 3
+/**
+ * 首页轮播图数据类型
+ */
+export type BannerItem = {
+  /** id */
+  id: string
+  /** 图片链接 */
+  imgUrl: string
+  /** 跳转链接 */
+  hrefUrl: string
+  /** 跳转类型 1: 页面 2: H5 3: 小程序 */
+  type: NavigateType
+}

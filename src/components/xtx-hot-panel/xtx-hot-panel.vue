1a836540d0cff2722c0cfadd1ca25262a6e84a6c
Simon <ooosaka1214@outlook.com>
2023-11-01 16:30:31 +0800

    feat(components): 封装热门推荐组件

---
+++ src/components/xtx-hot-panel/xtx-hot-panel.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <!-- 推荐专区 -->
+  <view class="panel hot">
+    <view class="item" v-for="item in 4" :key="item">
+      <view class="title">
+        <text class="title-text">特惠推荐</text>
+        <text class="title-desc">精选全攻略</text>
+      </view>
+      <navigator hover-class="none" url="/pages/hot/hot" class="cards">
+        <image
+          class="image"
+          mode="aspectFit"
+          src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_small_1.jpg"
+        ></image>
+        <image
+          class="image"
+          mode="aspectFit"
+          src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_small_2.jpg"
+        ></image>
+      </navigator>
+    </view>
+  </view>
+</template>
+
+<style lang="scss">
+/* 热门推荐 */
+.hot {
+  display: flex;
+  flex-wrap: wrap;
+  min-height: 508rpx;
+  margin: 20rpx 20rpx 0;
+  border-radius: 10rpx;
+  background-color: #fff;
+
+  .title {
+    display: flex;
+    align-items: center;
+    padding: 24rpx 24rpx 0;
+    font-size: 32rpx;
+    color: #262626;
+    position: relative;
+    .title-desc {
+      font-size: 24rpx;
+      color: #7f7f7f;
+      margin-left: 18rpx;
+    }
+  }
+
+  .item {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    height: 254rpx;
+    border-right: 1rpx solid #eee;
+    border-top: 1rpx solid #eee;
+    .title {
+      justify-content: start;
+    }
+    &:nth-child(2n) {
+      border-right: 0 none;
+    }
+    &:nth-child(-n + 2) {
+      border-top: 0 none;
+    }
+    .image {
+      width: 150rpx;
+      height: 150rpx;
+    }
+  }
+  .cards {
+    flex: 1;
+    padding: 15rpx 20rpx;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+</style>

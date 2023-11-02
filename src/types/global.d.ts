a17eb910fc99ec5727bc936ea23dbad9115bfff3
Simon <ooosaka1214@outlook.com>
2023-11-02 14:48:23 +0800

    feat(types): 添加通用分页参数类型声明

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -11,3 +11,11 @@ export type PageResult<T> = {
   /** 当前页数据 */
   items: T[]
 }
+
+/** 通用分页参数类型 */
+export type PageParams = {
+  /** 页码：默认值为 1 */
+  page?: number
+  /** 分页尺寸：默认值为 10 */
+  pageSize?: number
+}

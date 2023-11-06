f16478be7837abd1a178d94f7d6b2f7216b9ed80
Simon <ooosaka1214@outlook.com>
2023-11-06 17:19:17 +0800

    refactor: 获取商品详情数据接口请求函数追加具体类型，修改 goods 启动模式配置的启动参数

--- src/services/goods.ts
+++ src/services/goods.ts
@@ -1,11 +1,12 @@
 import request from '@/utils/request'
+import type { GoodsResult } from '@/types/goods'
 
 /**
  * 获取商品详情数据
  * @param id 商品id
  */
 export const getGoodsByIdApi = (id: string) =>
-  request({
+  request<GoodsResult>({
     method: 'GET',
     url: '/goods',
     data: { id },

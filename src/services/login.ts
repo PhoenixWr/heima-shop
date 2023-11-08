322ae32c4e4366bef8c9f5a55c5f0bca9734c5d8
Simon <ooosaka1214@outlook.com>
2023-11-08 16:12:55 +0800

    feat(services): 封装小程序登录内测接口请求

--- src/services/login.ts
+++ src/services/login.ts
@@ -20,3 +20,14 @@ export const postLoginWxMinApi = (data: LoginParams) =>
     url: '/login/wxMin',
     data,
   })
+
+/**
+ * 小程序登录-内测版
+ * @param phoneNumber 模拟手机号码
+ */
+export const postLoginWxMinSimpleApi = (phoneNumber: string) =>
+  request({
+    method: 'POST',
+    url: '/login/wxMin/simple',
+    data: { phoneNumber },
+  })
